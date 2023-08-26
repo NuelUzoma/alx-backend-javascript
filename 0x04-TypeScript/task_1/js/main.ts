2f8d0c6954d7069ded164788aa5910e6c46fe584
NuelUzoma <emcool17@gmail.com>
2023-08-26 03:29:21 +0100

    Task 0

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,3 +13,65 @@ const teacher: Teacher = {
   fullTimeEmployee: true,
   location: "Alabama",
 };
+
+teacher.yearsOfExperience = 10;
+teacher.contract = true;
+
+console.log(teacher);
+
+
+
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director: Directors = {
+  firstName: "Emmanuel",
+  lastName: "Uzo",
+  fullTImeEmployee: true,
+  location: "Maryland",
+  yearsOfExperience: 10,
+  numberOfReports: 7,
+}
+
+console.log(director);
+
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string,
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstInit = firstName.charAt(0).toUpperCase();
+  const lastInit = lastName.charAt(0).toUpperCase + lastName.slice(1);
+
+  return `${firstInit}. ${lastInit}`;
+};
+
+console.log(printTeacher("John", "Doe"));
+
+
+
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClass,
+}
+
+interface StudentInterface {
+  workOnHomework(): string,
+  displayName(): string,
+}
+
+class StudentClass {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
