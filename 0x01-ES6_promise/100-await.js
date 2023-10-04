b258b8e882ebca7af9269951869fd14c5094c5e4
NuelUzoma <emcool17@gmail.com>
2023-10-04 19:52:55 +0100

    Updates

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let res = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
