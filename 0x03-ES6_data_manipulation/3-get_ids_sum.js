a88c5a2f5139378220e8015ee5270bf3575677c2
NuelUzoma <emcool17@gmail.com>
2023-10-04 19:18:45 +0100

    Data Manipulation Task

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
